d23e00b53a25e624013a81614664c845bcbed738
iori <ab.ioryz@gmail.com>
2015-03-07 02:23:12 +0900

    change directory setting

---
+++ scripts/graph_util.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+
+def graph2dot(nodes, edges, edge_label, file_name="test.dot"):
+    file = open(file_name, 'w')
+    strs = ['digraph{',
+            'graph[rankdir=LR]',
+            nodes2dot(nodes),
+            edges2dot(edges, edge_label),
+            '}']
+    file.writelines(strs)
+    file.close()
+
+def nodes2dot(nodes):
+    s = ""
+    for node, shape in nodes.items():
+        print(shape, node)
+        # s += '\n{}[shape={}, label=\"{}\"];'.format(node, shape, node)
+        s += '\n{}[shape=\"{}\", label=\"{}\"];'.format(node, shape, node)
+    return s
+
+def edges2dot(edges, edge_label):
+    s = ""
+    for i, edge in enumerate(edges):
+        s += '\n{}->{}[label=\"{}\"];'.format(edge[0], edge[1], edge_label[i])
+    return s
+
+def make_graph(dfa, file_name="test.dot"):
+    print("making dot file for graphviz...")
+    rulebook = dfa.rulebook
+    accept_states = dfa.accept_states
+    nodes = {}
+    edges = []
+    edge_label = []
+    for rule in rulebook.rules:
+        nodes[rule.state] = 'circle'
+        nodes[rule.next_state] = 'circle'
+        # nodes.add(rule.state)
+        # nodes.add(rule.next_state)
+        edges.append([rule.state, rule.next_state])
+        edge_label.append(rule.character)
+    for accept_state in accept_states:
+        nodes[accept_state] = 'doublecircle'
+    graph2dot(nodes, edges, edge_label, file_name)
+
+    print("generating png file...")
+    cmd = "dot -Tpng -O {}".format(file_name)
+    os.system(cmd)
+    print("end.")
