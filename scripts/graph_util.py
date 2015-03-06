7ee81e893e9223c7cd04b47b462bd84f7f325475
iori <ab.ioryz@gmail.com>
2015-03-07 04:02:05 +0900

    apply to free transition dotted arrow

--- scripts/graph_util.py
+++ scripts/graph_util.py
@@ -4,12 +4,12 @@
 import sys
 import os
 
-def graph2dot(nodes, edges, edge_label, file_name="test.dot"):
+def graph2dot(nodes, edges, edge_label, edge_styles, file_name="test.dot"):
     file = open(file_name, 'w')
     strs = ['digraph{',
             'graph[rankdir=LR]',
             nodes2dot(nodes),
-            edges2dot(edges, edge_label),
+            edges2dot(edges, edge_label, edge_styles),
             '}']
     file.writelines(strs)
     file.close()
@@ -21,10 +21,10 @@ def nodes2dot(nodes):
         s += '\n{}[shape=\"{}\", label=\"{}\"];'.format(node, shape, node)
     return s
 
-def edges2dot(edges, edge_label):
+def edges2dot(edges, edge_label, edge_styles):
     s = ""
     for i, edge in enumerate(edges):
-        s += '\n{}->{}[label=\"{}\"];'.format(edge[0], edge[1], edge_label[i])
+        s += '\n{}->{}[style=\"{}\", label=\"{}\"];'.format(edge[0], edge[1], edge_styles[i], edge_label[i])
     return s
 
 def make_graph(fa, file_name="test.dot"):
@@ -34,16 +34,19 @@ def make_graph(fa, file_name="test.dot"):
     nodes = {}
     edges = []
     edge_label = []
+    edge_styles = []
     for rule in rulebook.rules:
         nodes[rule.state] = 'circle'
         nodes[rule.next_state] = 'circle'
-        # nodes.add(rule.state)
-        # nodes.add(rule.next_state)
         edges.append([rule.state, rule.next_state])
         edge_label.append(rule.character)
+        if not rule.character:
+            edge_styles.append('dotted')
+        else:
+            edge_styles.append('')
     for accept_state in accept_states:
         nodes[accept_state] = 'doublecircle'
-    graph2dot(nodes, edges, edge_label, file_name)
+    graph2dot(nodes, edges, edge_label, edge_styles, file_name)
 
     print("generating png file...")
     cmd = "dot -Tpng -O {}".format(file_name)
